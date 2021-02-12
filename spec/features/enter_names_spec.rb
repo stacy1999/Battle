5ebff13af03d1ee6da94922bf9eba89e758b4c24
Stacy1999 <anastasiapolinesi@gmail.com>
2021-02-12 10:26:38 +0000

    Refactored first two user stories

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
     scenario 'Submit names' do
-        visit('/')
-        fill_in :player_1_name, with: 'Anastasia'
-        fill_in :player_2_name, with: 'Luke'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Anastasia vs. Luke'
     end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
     scenario 'see Player 2 hit points' do
-        visit('/')
-        fill_in :player_1_name, with: 'Anastasia'
-        fill_in :player_2_name, with: 'Luke'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Luke: 60HP'
     end
 end
